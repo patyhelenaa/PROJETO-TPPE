a9045c0594dc9c26a5ca2b333fc424c1b5d2b2f3
Patyhelenaa <patyhelena160@gmail.com>
2025-06-11 15:49:09 -0300

    feat(models): adiciona models de usuarios

--- src/usuarios/models.py
+++ src/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    gravidez = models.BooleanField(default=False)
+    peso = models.FloatField()
+
+
+    class Meta:
+        db_table = 'Usuario'
+
+    def __str__(self):
+        return self.nome
+
