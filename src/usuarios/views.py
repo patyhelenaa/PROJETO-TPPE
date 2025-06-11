c3baa5d7f5063871eed42e8b5da86815bac69351
Patyhelenaa <patyhelena160@gmail.com>
2025-06-11 16:31:03 -0300

    feat(usuarios): adiciona UsuarioViewSet para operacoes CRUD

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
