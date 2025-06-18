3fa8141f83bff24b190d75876d2e3bf10bbbe26e
Patyhelenaa <patyhelena160@gmail.com>
2025-06-18 20:34:14 -0300

    feat(serializer): ajusta UsuarioSerializer para armazenar senhas com hash

--- src/usuarios/serializers.py
+++ src/usuarios/serializers.py
@@ -2,6 +2,24 @@ from rest_framework import serializers
 from .models import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    senha = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['id', 'nome', 'email', 'senha', 'data_nascimento', 'gravidez', 'peso']
+
+    def create(self, validated_data):
+        senha = validated_data.pop('senha')
+        usuario = Usuario(**validated_data)
+        usuario.set_password(senha)
+        usuario.save()
+        return usuario
+
+    def update(self, instance, validated_data):
+        senha = validated_data.pop('senha', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if senha:
+            instance.set_password(senha)
+        instance.save()
+        return instance
