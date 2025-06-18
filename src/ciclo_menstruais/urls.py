0127891975391a2df5581169885f3bbbd0847875
Patyhelenaa <patyhelena160@gmail.com>
2025-06-18 18:09:28 -0300

    [200~chore(urls): adiciona rotas do app 'sintomas' na URL base /api/

--- src/ciclo_menstruais/urls.py
+++ src/ciclo_menstruais/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
+    path('api/', include('sintomas.urls')),
 
 
 ]
