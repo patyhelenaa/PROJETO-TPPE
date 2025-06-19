35bf419ecb4c29262c07f75090e0951420124b8f
Patyhelenaa <patyhelena160@gmail.com>
2025-06-19 16:49:57 -0300

    feat(models): cria modelo Ciclo com campos de ciclo menstrual

--- src/ciclos/models.py
+++ src/ciclos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from usuarios.models import Usuario  
 
-# Create your models here.
+class Ciclo(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='ciclos')
+    data = models.DateField() 
+    dia_menstruada = models.BooleanField()
+    duracao_ciclo = models.PositiveIntegerField()  
+    duracao_menstruacao = models.PositiveIntegerField() 
+    fluxo_menstrual = models.CharField(max_length=20, choices=[
+        ('LEVE', 'Leve'),
+        ('MODERADO', 'Moderado'),
+        ('INTENSO', 'Intenso'),
+        ('MUITO_INTENSO', 'Muito Intenso'),
+    ])
+
+    def __str__(self):
+        return f"Ciclo do usuário {self.usuario.nome} em {self.data}"
+
+    class Meta:
+        db_table = 'ciclo'
