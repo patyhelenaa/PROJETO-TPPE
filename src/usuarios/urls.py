265777fc6e41df4823938693e8d27e69a49bb067
Patyhelenaa <patyhelena160@gmail.com>
2025-06-19 16:55:27 -0300

    fix(urlUsuarios): adiciona basename no UsuarioViewSet no router

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet
+from ciclos.views import CicloViewSet
 
 router = DefaultRouter()
-router.register(r'usuarios', UsuarioViewSet)
+router.register(r'usuarios', UsuarioViewSet, basename='usuario')
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
