4617ae8b31823476f81db042014ccf94961d7cc5
Patyhelenaa <patyhelena160@gmail.com>
2025-06-19 16:50:59 -0300

    feat(serializers): cria serializer para o modelo Ciclo

---
+++ src/ciclos/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Ciclo
+
+class CicloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ciclo
+        fields = ['id', 'usuario', 'data', 'dia_menstruada', 'duracao_ciclo', 'duracao_menstruacao', 'fluxo_menstrual']
