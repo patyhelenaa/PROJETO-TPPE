1044afabe7786cd837726a634fa6e0eff729c0f4
Patyhelenaa <patyhelena160@gmail.com>
2025-07-19 06:16:18 -0300

    feat(sintomas): adiciona campo ciclo_data nos serializers para exibir data de início do ciclo nos sintomas

--- src/sintomas/serializers.py
+++ src/sintomas/serializers.py
@@ -8,10 +8,19 @@ class FisicoSerializer(serializers.ModelSerializer):
         queryset=Ciclo.objects.all(),
         required=False
     )
+    ciclo_data = serializers.SerializerMethodField()
 
     class Meta:
         model = Fisico
         fields = '__all__'
+
+    def get_ciclo_data(self, obj):
+        return obj.ciclo.data if obj.ciclo else None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['ciclo_data'] = self.get_ciclo_data(instance)
+        return rep
 
 
 class HumorSerializer(serializers.ModelSerializer):
@@ -19,10 +28,19 @@ class HumorSerializer(serializers.ModelSerializer):
         queryset=Ciclo.objects.all(),
         required=False
     )
+    ciclo_data = serializers.SerializerMethodField()
 
     class Meta:
         model = Humor
         fields = '__all__'
+
+    def get_ciclo_data(self, obj):
+        return obj.ciclo.data if obj.ciclo else None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['ciclo_data'] = self.get_ciclo_data(instance)
+        return rep
 
 
 class LibidoSerializer(serializers.ModelSerializer):
@@ -30,10 +48,19 @@ class LibidoSerializer(serializers.ModelSerializer):
         queryset=Ciclo.objects.all(),
         required=False
     )
+    ciclo_data = serializers.SerializerMethodField()
 
     class Meta:
         model = Libido
         fields = '__all__'
+
+    def get_ciclo_data(self, obj):
+        return obj.ciclo.data if obj.ciclo else None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['ciclo_data'] = self.get_ciclo_data(instance)
+        return rep
 
 
 class SecrecaoSerializer(serializers.ModelSerializer):
@@ -41,7 +68,16 @@ class SecrecaoSerializer(serializers.ModelSerializer):
         queryset=Ciclo.objects.all(),
         required=False
     )
+    ciclo_data = serializers.SerializerMethodField()
 
     class Meta:
         model = Secrecao
         fields = '__all__'
+
+    def get_ciclo_data(self, obj):
+        return obj.ciclo.data if obj.ciclo else None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['ciclo_data'] = self.get_ciclo_data(instance)
+        return rep
